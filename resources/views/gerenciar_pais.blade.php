466b706eac4b5b3b2070977d521de2fdb1125996
Pedro Luis Freitas da Rocha <pedro.luis@academico.ifrn.edu.br>
2018-12-17 13:12:14 -0300

    tela gerenciar paisalterada

--- resources/views/gerenciar_pais.blade.php
+++ resources/views/gerenciar_pais.blade.php
@@ -2,13 +2,34 @@
 
 @section('conteudo')
 
-<h1>Gerenciar Pais</h1>
+<h1 h1 align="center">Gerenciar Pais</h1>
 
-<p><a href="/pais_cadastrar">Cadastrar novo Pai</a></p>
+<div class="row">
 
-<p>Pais Cadastrados:</p>
+	<div class="col-md-3"></div>
+
+	<div class="col-md-6">
+
+<p align="center"><a input class="bnt" name="" type="submit" href="/pais_cadastrar">Cadastrar novo Pai</a></p>
+
+<h3 align="center">Pais Cadastrados:</h3>
 @foreach($pais as $p)
-<p>{{$p->nome}} | <a href="/pais_editar/{{$p->id}}"">EDITAR</a> | <a href="/pais_remover/{{$p->id}}">REMOVER</a></p>
-@endforeach
 
+
+<div class="row">
+
+	<div class="col-md-6" align="right">
+		<h5>{{$c->nome}}</h5>
+	</div>
+
+	<div class="col-md-6" align="left">
+
+<p> | <a href="/pais_editar/{{$p->id}}"">EDITAR</a> | <a href="/pais_remover/{{$p->id}}">REMOVER</a></p>
+
+		</div>
+	</div>
+
+@endforeach
+			</div>
+	</div>
 @endsection
